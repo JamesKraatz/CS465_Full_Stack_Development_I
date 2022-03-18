8004b57b57260799f6bebe2b107ab791930f8381
Jimmie Kraatz <james.kraaz@snhu.edu>
2022-03-18 18:34:01 -0500

    Convert travel template from static data to dynamic JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,9 +1,13 @@
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
+/* GET traveler view */
 const travel = (req, res) => {
     const package = require("../../package.json");
     pageTitle = package.description + ' - Travel';
-    res.render('travel', {title: pageTitle});
+    res.render('travel', {title: pageTitle, trips});
 };
 
 module.exports = {
-    travel
+    travel,
 };
